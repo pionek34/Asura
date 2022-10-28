9331078f87cc0030d571ba8d953cf044e35d0d93
LastKnell <yatharthrathi.rathi@gmail.com>
2022-10-29 03:10:12 +0530

    NOW FINALLY FIXED

--- src/main/java/dev/lastknell/core/methods/impl/Ping.java
+++ src/main/java/dev/lastknell/core/methods/impl/Ping.java
@@ -2,6 +2,7 @@ package dev.lastknell.core.methods.impl;
 
 import dev.lastknell.core.NettyBootstrap;
 import dev.lastknell.core.methods.IMethod;
+import dev.lastknell.core.packets.Handshake;
 import dev.lastknell.core.packets.PingPacket;
 import dev.lastknell.core.proxy.Proxy;
 import io.netty.buffer.Unpooled;
@@ -9,13 +10,14 @@ import io.netty.channel.Channel;
 
 public class Ping implements IMethod{
 
-    private PingPacket packet;
     private NettyBootstrap service;
-
+    private byte[] handshakebytes;
     @Override
     public void accept(Channel channel, Proxy proxy) {
         service.oppnedCPS++;
-        channel.writeAndFlush(Unpooled.buffer().writeBytes(packet.getWrappedPacket()));
+        channel.writeAndFlush(Unpooled.buffer().writeBytes(this.handshakebytes));
+        channel.writeAndFlush(Unpooled.buffer().writeBytes(new byte[] { 1, 0 }));
+        channel.writeAndFlush(Unpooled.buffer().writeBytes((new PingPacket(System.currentTimeMillis())).getWrappedPacket()));
         channel.close();
         service.successfulCPS++;
     }
@@ -23,6 +25,7 @@ public class Ping implements IMethod{
     @Override
     public void init(NettyBootstrap service) {
         this.service = service;
+        handshakebytes = new Handshake(service.protocolID, service.srvIp, service.port, 1).getWrappedPacket();
     }
 
     @Override
